fcdd6d5331237fcd77d898a94d2436f229887eb2
pnelslkj <57844062+pnelslkj@users.noreply.github.com>
2019-11-17 11:38:00 +0530

    save

---
+++ Booklist/04Django/04As_a_Backend/05Part3/02app_urls.py
@@ -0,0 +1,14 @@
+# blog/urls.py
+from django.urls import path
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+)
+
+urlpatterns = [
+    path('post/new/', BlogCreateView.as_view(), name='post_new'), # new
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path(', BlogListView.as_view(), name='home'),
+
+]
---
+++ Booklist/04Django/04As_a_Backend/05Part3/03app_views.py
@@ -0,0 +1,18 @@
+# blog/views.py
+from django.views.generic import ListView, DetailView # new
+from django.views.generic.edit import CreateView # new
+
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
